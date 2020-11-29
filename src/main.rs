2596a706f292a14c14601234e821e41f85bdfd01
Tim Small <tim@seoss.co.uk>
2020-11-29 17:59:51 +0000

    Use the higher precision crystal oscillator on the Nucleo 64
    Previously the built-in (low precision) oscillator was used to create
    the core clock for the MCU.  This was mostly because I (Tim Small) had
    made some changes to the on-board debugger on my Nucleo which had the
    side effect of disabling the crystal derived clock signal being passed
    from the debugger on to the MCU.

--- src/main.rs
+++ src/main.rs
@@ -44,9 +44,14 @@ const APP: () = {
         // Device specific peripherals
         let dp: stm32::Peripherals = cx.device;
 
-        // Set up the system clock
+        // Set up the system clock, using the Nucleo's 8MHz crytal
+        // instead of the (less accurate) internal oscillator.
         let rcc = dp.RCC.constrain();
-        let clocks = rcc.cfgr.sysclk(CORE_CLOCK_MHZ.mhz()).freeze();
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.mhz())
+            .sysclk(CORE_CLOCK_MHZ.mhz())
+            .freeze();
 
         // Initialize (enable) the monotonic timer (CYCCNT)
         cx.core.DCB.enable_trace();
